98cd467e7313c57168c72a383490419b6755b593
John Doe <johndoe@email.com>
2024-04-10 20:16:13 +0530

    update socketProvider

--- client/src/providers/SocketProvider.jsx
+++ client/src/providers/SocketProvider.jsx
@@ -11,7 +11,7 @@ export const useSocket=()=>{
 }
 
 export const SocketProvider=(props)=>{
-    const socket=useMemo(()=>io("http://localhost:8000/"),[])
+    const socket=useMemo(()=>io("https://video-meet-server-brown.vercel.app/"),[])
     return(
         <socketContext.Provider value={socket}>
             {props.children}
